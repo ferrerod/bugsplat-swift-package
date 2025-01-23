fae839606a648ada4bd654734243716104165c5d
David Ferrero <david.ferrero@zion.com>
2025-01-23 14:23:23 -0700

    Initial BugSplat Swift Package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// Bundle up BugSplat.xcframework as a Swift Package suitable for integration with Swift Package Manager
+
+let package = Package(
+    name: "BugSplat",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(
+            name: "BugSplat",
+            targets: ["BugSplat"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BugSplat",
+            url: "https://github.com/ferrerod/bugsplat-swift-package/Releases/BugSplat.xcframework.1.0.0.zip",
+            checksum: "4f33c5d70379b90656adf3eac1a8cd823ebea910344e0da2343cf67231465e7f"
+        ),
+        // Add a fake target to satisfy the swift build system
+        // Add a dependency to the .binaryTarget
+        // Add the expected Sources folder structure: Sources/BugSplatPackage/
+        // Add a fake Swift source: Sources/BugSplatPackage/Empty.swift
+        .target(
+            name: "BugSplatPackage",
+            dependencies: ["BugSplat"]
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,14 @@
+## Introduction
+
+This repository contains the necessasry structure and Package.swift to allow BugSplat.xcframework to be integrated into a Swift project for iOS, macOS, and Mac Catalyst applications.
+
+## 1. Requirements
+
+* XCode with Swift 5.3 or newer.
+
+
+# FIXME
+
+In the Package.swift, the url for BugSplat binaryTarget is currently set to:
+url: "https://github.com/ferrerod/bugsplat-swift-package/Releases/BugSplat.xcframework.1.0.0.zip"
+It should be hosted in a suitable location where versioned releases are managed.
